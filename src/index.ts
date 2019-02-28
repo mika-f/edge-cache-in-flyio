ed5ca1f46f164358f13fa06181ae85915a26ca88
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2019-02-28 23:15:40 +0900

    feat: caching for my blog

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,34 @@
+import { responseCache } from "@fly/cache";
+
 declare const fly: any;
 
-fly.http.respondWith(async (req: any) => {
-  return new Response('Redirecting', {
-    headers: {
-      'Location': 'https://fly.io/docs/apps/'
-    },
-    status: 302
-  });
+fly.http.respondWith(async (req: RequestInfo) => {
+  const url = new URL((req as any).url);
+
+  let resp: Response = await responseCache.get(url.pathname);
+  if (resp) {
+    // hit
+    resp.headers.set('Edge-Cache', 'Hit');
+    return resp;
+  }
+
+  // no hit
+  resp = await fetch(`https://blog.mochizuki.moe${url.pathname}`);
+
+  const type = resp.headers.get('content-type');
+  if (!type) {
+    // what is this?
+    resp.headers.set('Edge-Cache', 'Ignored');
+    return resp;
+  }
+
+  if (!type.includes("text/html")) {
+    // no cache in edge location (cached in Service Worker)
+    resp.headers.set('Edge-Cache', 'Ignored');
+    return resp;
+  }
+
+  await responseCache.set(url.pathname, resp, 3600);
+  resp.headers.set('Edge-Cache', 'Origin');
+  return resp;
 });
